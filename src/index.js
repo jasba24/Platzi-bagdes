af09c2fc7d364c7e4d4e6d930fbb9b2a99b2a3ca
jasba24 <johanbuitrago342@gmail.com>
2021-03-30 16:47:19 -0500

    example for show the use of react with jsx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// const element = document.createElement("h1")
+// element.innerHTML = "Hello, Platzi Badges"
 
-// const container = document.getElementById('app');
+// const container = document.getElementById("app")
 
-// container.appendChild(element);
+// container.appendChild(element)
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const urlLink = "https://platzi.com"
 
-const container = document.getElementById('app');
+// const jsx = <h1>Hello, Platzi badges from React!</h1>
+// const element = React.createElement("a", {href: `${urlLink}`, target: "_blank"}, "Ir a platzi")
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+const name = "angela"
+
+// const jsx = <h1>Hola soy {4+4}</h1>
+
+const container = document.getElementById("app")
+
+const jsx = <div>
+	<h1>Hola soy Richard</h1>
+	<p>Soy ingeniero fronted.</p>
+</div>
+
+// ReactDOM.render(__qué__, __dónde__)
+
+ReactDOM.render(jsx, container)
