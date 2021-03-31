4b368b2dd8c2354171ad5a58cba387ab52b77a0d
jasba24 <johanbuitrago342@gmail.com>
2021-03-31 17:15:15 -0500

    form with new events, for get values, clicks and submits

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,37 @@
+import React, { Component} from "react"
+
+
+class BadgeForm extends Component {
+	handleSubmit = e => {
+		e.preventDefault()
+	}
+
+	handleChange = e => {
+		console.log({
+			name: e.target.name,
+			value: e.target.value
+		});
+	}
+
+	handleClick = e => {
+		console.log("button was clicked");
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>New Attendant</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<label>First Name</label>
+						<input onChange={this.handleChange} className="form-control" name="firstName"/>
+					</div>
+
+					<button onClick={this.handleClick} className="btn btn-primary">Save</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default BadgeForm
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import "./styles/BadgeNew.css"
 import header from "../images/badge-header.svg"
 import NavBar from "../components/NavBar"
 import Badge from "../components/Badge"
+import BadgeForm from "../components/BadgeForm"
 
 class BadgeNew extends React.Component {
 	render() {
@@ -15,8 +16,11 @@ class BadgeNew extends React.Component {
 
 				<div className="container">
 					<div className="row">
-						<div className="col">
+						<div className="col-6">
 							<Badge firstName="Richard" lastName="Kaufman" twitter="sparragus" jobTitle="Fronted Enginner" avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"/>
+						</div>
+						<div className="col-6">
+							<BadgeForm/>
 						</div>
 					</div>
 				</div>
