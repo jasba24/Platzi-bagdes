a89ca9b055f8fd5ae6fcd61bd732411aaeeb829f
jasba24 <johanbuitrago342@gmail.com>
2021-03-31 15:21:40 -0500

    import bootstrap, and add the use of the props in the components

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -6,6 +6,8 @@ import confLogo from "../images/badge-header.svg"
 
 class Badge extends React.Component {
 	render() {
+		const {firstName, lastName, jobTitle, twitter} = this.props  
+
 		return (
 			<div className="Badge">
 				<div className="Badge__header">
@@ -15,12 +17,12 @@ class Badge extends React.Component {
 
 				<div className="Badge__section-name">
 					<img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
-					<h1>Richard <br/> Kaufman</h1>
+					<h1>{firstName} <br/> {lastName}</h1>
 				</div>
 
 				<div className="Badge__section-info">
-					<h3>Fronted Enginner</h3>
-					<div>@sparragus</div>
+					<h3>{jobTitle}</h3>
+					<div>@{twitter}</div>
 				</div>
 
 				<div className="Badge__footer">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import md5 from "md5"
+import "bootstrap/dist/css/bootstrap.css"
 
-import "./bootstrap"
-
+import "./global.css"
 import Badge from "./components/Badge"
 
 const container = document.getElementById("app")
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(
+	<Badge
+		firstName="Richard"
+		lastName="Kaufman"
+		jobTitle="Frontend Enginner"
+		twitter="sparragus"
+	/>, container)
