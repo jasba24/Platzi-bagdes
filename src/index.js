deebacb3eb6409b442d9026928e5d6781cfc5902
jasba24 <johanbuitrago342@gmail.com>
2021-03-30 20:10:36 -0500

    new components with styles and add at index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-// const element = document.createElement("h1")
-// element.innerHTML = "Hello, Platzi Badges"
-
-// const container = document.getElementById("app")
-
-// container.appendChild(element)
-
 import React from "react"
 import ReactDOM from "react-dom"
 
-// const urlLink = "https://platzi.com"
+// import "./bootstrap"
 
-// const jsx = <h1>Hello, Platzi badges from React!</h1>
-// const element = React.createElement("a", {href: `${urlLink}`, target: "_blank"}, "Ir a platzi")
-
-const name = "angela"
-
-// const jsx = <h1>Hola soy {4+4}</h1>
+import Badge from "./components/Badge"
 
 const container = document.getElementById("app")
 
-const jsx = <div>
-	<h1>Hola soy Richard</h1>
-	<p>Soy ingeniero fronted.</p>
-</div>
-
-// ReactDOM.render(__qué__, __dónde__)
-
-ReactDOM.render(jsx, container)
+ReactDOM.render(<Badge />, container)
