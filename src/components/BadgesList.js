91b1e271b1e4731c0e5c1a49bdff3aa98a116c4c
jasba24 <johanbuitrago342@gmail.com>
2021-03-31 20:28:20 -0500

    add info example

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 export class BadgesList extends Component {
-	render() {
-		return (
-			<ul className="list-unstyled">
-              {this.props.badges.map(badge => {
-                return (
-                  <li key={badge.id}>
-                    <p>
-                      {badge.firstName} {badge.lastName}
-                    </p>
-                  </li>
-                )
-              })}
-            </ul>
-		)
-	}
+  render() {
+    return (
+      <ul className="list-unstyled">
+        {this.props.badges.map(badge => {
+          return (
+            <li key={badge.id}>
+              <div>
+                <img src={badge.avatarUrl} alt=""/>
+              </div>
+              <div>
+                <h1>
+                  {badge.firstName} {badge.lastName}
+                </h1>
+                <a>
+                  <img src="" alt="" /> <span>@{badge.twitter}</span>
+                </a>
+                <p>{badge.jobTitle}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
 }
 
 export default BadgesList
